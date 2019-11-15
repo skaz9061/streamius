54e104bf82174dd8779ab0a967a71916be5a1601
Steven Kazmierkiewicz <stevekaz@gmail.com>
2019-11-15 18:58:10 -0500

    style touch ups

--- streamius/src/app/app.component.ts
+++ streamius/src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'streamius';
+  subtitle = 'a single search spanning several streaming services';
 }
--- streamius/src/app/components/searchBar/searchBar.component.ts
+++ streamius/src/app/components/searchBar/searchBar.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'search-bar',
   templateUrl: './searchBar.component.html',
-  styleUrls: ['../../app.component.css', './searchBar.component.css']
+  styleUrls: ['./searchBar.component.css']
 })
 export class SearchBarComponent {
   searchString: string = "";
